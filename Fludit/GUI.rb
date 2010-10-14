b45a7765374c75cfd4c1ef3ece54d753c060271e
Thomas Krisch <tak017@bucknell.edu>
2010-10-14 17:03:13 -0400

    Still working on the gui, its functional but the colors are messed up.  Something to do with the colormap, I have no idea

--- Fludit/GUI.rb
+++ Fludit/GUI.rb
@@ -5,44 +5,47 @@ class GUI
 	def initialize(grid, colors)
     @grid = grid
     @colors = [
-              Gdk::Color.new(0,0,0),
-              Gdk::Color.new(255,255,255),
-              Gdk::Color.new(255,0,0),
-              Gdk::Color.new(0,255,0),
-              Gdk::Color.new(0,0,255),
-              Gdk::Color.new(125,125,125)]
-  
+              Gdk::Color.parse("Black"),
+              Gdk::Color.parse("White"),
+              Gdk::Color.parse("Blue"),
+              Gdk::Color.parse("Yellow"),
+              Gdk::Color.parse("Red"),
+              Gdk::Color.parse("Green")]
+    @width = @grid.size
+    @height = @grid[0].size
+
+    @rect_size = 60
+
   end
 
   def display
     Gtk.init
     @gw = Gtk::Window.new("Fludit!")
-    @gw.resize(400,400)
-    
-    @gw.show
-    
+    @gw.resize(@rect_size * @width,@rect_size * @height)    
     @gw.signal_connect('delete_event') { Gtk.main_quit }
 
     @area = Gtk::DrawingArea.new
-    @area.set_size_request(240,240)
+    @area.set_size_request(@rect_size * @width,@rect_size * @height)
+    @area.signal_connect('expose_event')        { |w, e| redraw_board }
 
     @gw.add(@area)
-    @gw.showall
-  
-    update
+    @gw.show_all
+
+    @gc = Gdk::GC.new(@area.window)
+    @gc.function = Gdk::GC::COPY
+
+    @gc.colormap = Gdk::Colormap.system
 
     Gtk.main
   end
     
-  def update
-  
+  def redraw_board
     @grid.each_with_index do |r, x|
       r.each_with_index do |c, y|
-        @area.style = @colors[c]
-        @area.window.draw_rectangle(@gc,true,x,y,20,20)
+        @gc.set_foreground( @colors[c] )
+        @area.window.draw_rectangle(@gc,true,x*@rect_size,y*@rect_size,@rect_size,@rect_size)
+        #puts "Drawing rectangle (#{x},#{y}) ==> #{@colors[c].to_s} ==> #{@gc.foreground}"
       end
     end
-
-    #@gw.draw_drawable(@gc, @buffer,0,0,0,0,-1,-1)
   end
 end
